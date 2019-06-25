5957be1ef16c5f47d5b1fc082a7d6ba510604241
kingOyaro <42372824+kingOyaro@users.noreply.github.com>
2019-06-25 12:54:57 +0300

    Add files via upload

---
+++ messages.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="logsuccess.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+<div class="cong">
+    <form>
+        <h2 style="color: white">Order placed successfully</h2>
+        <a href="profile.php" style="text-decoration: none">Click here to order</a>
+        <br><br>
+    </form>
+    
+    </div>
+
+</body>
+</html>
--- order.php
+++ order.php
@@ -1,17 +1,44 @@
+
 <?php
+
 // define variables and set to empty values
 	$large = ($_POST["largepizza"]);
   	$small = ($_POST["smallpizza"]);
   	$medium = ($_POST["mediumpizza"]);
   	$toppings = ($_POST["tops"]);
+  	$donation = ($_POST["donations"]);
+
+  	if (empty($_POST["largepizza"])){
+	$large=0;
+	}
+
+  	if (empty($_POST["smallpizza"])){
+	$small=0;
+	}
+
+  	if (empty($_POST["mediumpizza"])){
+	$medium=0;
+	}
+
+	if (empty($_POST["tops"])){
+	$toppings=0;
+	}
+
+	if (empty($_POST["donations"])){
+	$donation=0;
+	}
+
 function totals(){
 	#$GLOBALS['large','small','medium','toppings'];
 	$large = ($_POST["largepizza"]);
   	$small = ($_POST["smallpizza"]);
   	$medium = ($_POST["mediumpizza"]);
-	$sum = $large*1100+$small*800+$medium*550;
+  	$donation = ($_POST["donations"]);
+  	$sum=0;
+	$sum = $sum+$large*1100+$small*800+$medium*550+$donation;
 	return $sum;
 }
+
 
 if ($toppings == "Meat"){
 
@@ -30,63 +57,59 @@ $Totalcost = totals();
 if($large<0 ||$small<0||$medium<0){
 	$Totalcost="Invalid entry";
 }
-
-/*
-function totals($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}*/
 ?>
+<!--<div id="msg" name="message">Congratulations You Sign Up successfully!!</div>-->
 
 <?php
-
-#function to connect to the database
 function connect(){
-		$dbserver = "localhost";
-		$dbuser = "root";
-		$dbpass = "";
-		$dbname = "pizza_system";
-		$link  = mysqli_connect($dbserver,$dbuser,$dbpass,$dbname) or die("Could not connect");
-		return $link;
-	}
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "webregistration";
+	$link=
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    return $link;
+}}
+session_start();
 
-#function to fetch data from sql
-function getData($sql){
+function setData($sql_registerform){ 
+	$link = connect();
+	if(mysqli_query($link,$sql)){
+		 $last_id = mysqli_insert_id($link);
+		  return $last_id;
+	 }
+	else{ return false; }}
+$date_created=date('Y-m-d h:i:s');
 
-		$link = connect();
-		$result = mysqli_query($link,$sql);
-		$rows = array();
-		while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-			$rows[] = $row;
-		}
-		return $rows;
-	}
+#$sql = "INSERT INTO trial1 (Name, Email) VALUES ('".$yourFirstName."', '".$yourEmail."')";
+$yourstatus = 0;
+$sql = "INSERT INTO `orders`(`user_id`,`date_created`, `amount`, `status`,`donation`) VALUES ('".$last_id."','".$date_created."', '".$Totalcost."','".$yourstatus."')";
 
-	
-#function to fetch last id entered to database
-	function setData($sql){
-		
-		$link = connect();
+if ($conn->query($sql) === TRUE) {
+    #echo "Registration Complete";
+	header("Location: message.php");
 
-		if(mysqli_query($link,$sql)){
-			$last_id = mysqli_insert_id($link);
-			return $last_id;
-		}else{
-			return false;
-		}
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
-
-	}
+$conn->close();
 
 ?>
+
+
+
+
+
 
 <!DOCTYPE html>
 <html>
-	<head>
-	</head>
+	<head> </head>
 	<style type="text/css">
 		body
 {
@@ -136,22 +159,32 @@ h2
 	
 	<body>
 		<div id='checkout'>
-			<a style="text-decoration: none">Here is your order <?php
+			<a style="text-decoration: none">Here is your order<br> 
+			<?php
 			if($large<0 ||$small<0||$medium<0){
 				echo "<br>";
 				$Totalcost="Invalid entry. Order cannot be negative. Re-order";
 				echo $Totalcost;
 				}
-			else if($large>=0 ||$small>=0||$medium>=0){
+
+			else if ($Totalcost<201){
+				$err="Invalid entry.Please select an Item";
+				echo $err;
+			}
+
+			else if($large>0 ||$small>0||$medium>0){
 				echo "<br>";
 				echo $large; 
-				echo "Large Pizza";
+				echo " Large Pizza";
 				echo "<br>";
 				echo $small;
-				echo "Small Pizza";
+				echo " Small Pizza";
 				echo "<br>";
 				echo $medium;
-				echo "Medium Pizza";
+				echo " Medium Pizza";
+				echo "<br>";
+				echo "Donation ";
+				echo $donation;
 				echo "<br>";
 				echo "Your total is";
 				echo " Ksh ";
